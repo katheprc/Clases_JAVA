bc30d6e3b369df9e99c489c6af3be029ca16edf0
katheprc <yaelvaldez@gmail.com>
2023-09-18 16:10:10 -0300

    -

---
+++ src/Excepciones/Ejercicio3/DivisionNumero.java
@@ -0,0 +1,11 @@
+package Excepciones.Ejercicio3;
+
+public class DivisionNumero {
+
+	public double dividir(String num1, String num2) throws Exception{
+		
+		return(Integer.parseInt(num1)/Integer.parseInt(num2));
+		
+	}
+	
+}
---
+++ src/Excepciones/Ejercicio3/Ejercicio3_main.java
@@ -0,0 +1,35 @@
+package Excepciones.Ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3_main {
+
+	public static void main(String[] args) {
+
+		DivisionNumero divNum = new DivisionNumero();
+
+		Scanner leer = new Scanner(System.in);
+
+		do {
+
+			try {
+
+				String num1 = leer.nextLine();
+				String num2 = leer.nextLine();
+				
+				System.out.println(num1 + "/" + num2 + " = " + divNum.dividir(num1, num2));
+
+			} catch (Exception ex) {
+
+				System.out.println("Error 1: " + ex);
+				System.out.println("------------------------------------------------");
+				System.out.println("Error 2: " + ex.getMessage());
+				System.out.println("------------------------------------------------");
+				System.out.println("Error 3: " + ex.fillInStackTrace());
+
+			}
+			
+		} while (true);
+	}
+
+}
