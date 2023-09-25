fcbd49d68a51ff883ed758d5fde477f65b5ac6b0
katheprc <yaelvaldez@gmail.com>
2023-09-25 15:15:19 -0300

    -

--- src/mysql/script ejercicio1.sql
+++ src/mysql/script ejercicio1.sql
@@ -66,21 +66,86 @@ where nombre like "J%";
 
 
 -- 14. Listar el salario, la comisión, el salario total (salario + comisión) y nombre, de aquellos empleados que tienen comisión superior a 1000.
+
 select nombre, sal_emp, comision_emp, (sal_emp + comision_emp) as Total from empleados
 where comision_emp > 1000;
 
 -- 15. Obtener un listado similar al anterior, pero de aquellos empleados que NO tienen comisión.
--- 16. Obtener la lista de los empleados que ganan una comisión superior a su sueldo.
--- 17. Listar los empleados cuya comisión es menor o igual que el 30% de su sueldo.
--- 18. Hallar los empleados cuyo nombre no contiene la cadena “MA”
--- 19. Obtener los nombres de los departamentos que sean “Ventas”, “Investigación” o ‘Mantenimiento.
--- 20. Ahora obtener el contrario, los nombres de los departamentos que no sean “Ventas” ni “Investigación” ni ‘Mantenimiento.
 
+select nombre, sal_emp, comision_emp, (sal_emp + comision_emp) as Total from empleados
+where comision_emp = 0;
+
+-- 16. Obtener la lista de los empleados que ganan una comisión superior a su sueldo.
+
+select * from empleados 
+where comision_emp > sal_emp;
+
+
+-- 17. Listar los empleados cuya comisión es menor o igual que el 30% de su sueldo.
+select * from empleados
+where comision_emp <= (sal_emp*0.30);
+
+
+-- 18. Hallar los empleados cuyo nombre no contiene la cadena “MA”
+
+select * from empleados
+where nombre not like "%MA%"
+
+
+-- 19. Obtener los nombres de los departamentos que sean “Ventas”, “Investigación” o ‘Mantenimiento.
+
+select nombre_depto from departamentos
+where nombre_depto = "VENTAS" or nombre_depto = "INVESTIGACIÓN" or nombre_depto = "MANTENIMIENTO"; 
+
+
+-- 20. Ahora obtener el contrario, los nombres de los departamentos que no sean “Ventas” ni “Investigación” ni ‘Mantenimiento.
+select nombre_depto from departamentos
+where nombre_depto != "VENTAS" AND nombre_depto != "INVESTIGACIÓN" AND nombre_depto != "MANTENIMIENTO"; 
 
 -- 21. Mostrar el salario más alto de la empresa.
 select max(sal_emp) from empleados;
 
 
 -- 22. Mostrar el nombre del último empleado de la lista por orden alfabético.
+select nombre from empleados
+order by nombre desc
+limit 1;
+
+
 -- 23. Hallar el salario más alto, el más bajo y la diferencia entre ellos.
+
+select max(sal_emp) as Maximo, min(sal_emp) as Minimo, (max(sal_emp)-min(sal_emp)) as Diferencia from empleados;
+
+
 -- 24. Hallar el salario promedio por departamento.
+select id_depto, avg(sal_emp) from empleados
+group by id_depto;
+
+
+-- Consultas con Having
+-- 25. Hallar los departamentos que tienen más de tres empleados. Mostrar el número de empleados de esos departamentos.
+select id_depto, count(id_emp) as Cantidad from empleados 
+group by id_depto having Cantidad > 3;
+
+
+-- 26. Hallar los departamentos que no tienen empleados
+select id_depto, count(id_emp) as Cantidad from empleados
+group by id_depto having Cantidad = 0;
+
+
+-- Consulta con Subconsulta
+-- 28. Mostrar la lista de los empleados cuyo salario es mayor o igual que el promedio de la empresa. Ordenarlo por departamento.
+select * from empleados
+where sal_emp >= (select avg(sal_emp) from empleados)
+order by id_depto;
+
+
+
+
+-- ejemplo mostrar todos los empelados cuyo id sea mayor o igual a la cantidad de empleados
+select * from empleados
+where id_emp <= (select count(id_emp) from empleados);
+
+
+
+
