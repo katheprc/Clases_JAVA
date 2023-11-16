d83ac5152a73260147b35130c83174e24b71a9f3
katheprc <yaelvaldez@gmail.com>
2023-11-16 11:25:26 -0300

    -

--- src/spring boot/TP1_Maria/src/main/java/com/katheprc/TP1/controller/ProfesorController.java
+++ src/spring boot/TP1_Maria/src/main/java/com/katheprc/TP1/controller/ProfesorController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.katheprc.TP1.entity.Profesor;
 import com.katheprc.TP1.service.ProfesorService;
@@ -17,20 +18,38 @@ public class ProfesorController {
 
 	@Autowired
 	ProfesorService pSrv = new ProfesorService();
-	
-	
+
 	@GetMapping("/")
 	public String home() {
+		List<Profesor> listaProfes = pSrv.listar();
+
+		model.addAttribute("listaProfes", listaProfes);
+
 		return "index.html";
 	}
-	
+
 	@GetMapping("/listar")
 	public String listar(Model model) {
-		
+
 		List<Profesor> listaProfes = pSrv.listar();
-		
+
 		model.addAttribute("listaProfes", listaProfes);
-		
+
 		return "listar.html";
 	}
+
+	@GetMapping("/guardar")
+	public String guardar(Model model) {
+		return "guardar.html";
+	}
+
+	@GetMapping("/guardar/exitoso")
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("materia") String materia,
+			@RequestParam("turno") String turno) {
+
+		pSrv.guardar(nombre, materia, turno);
+
+		return "guardar.html";
+	}
+
 }
--- src/spring boot/TP1_Maria/src/main/java/com/katheprc/TP1/service/ProfesorService.java
+++ src/spring boot/TP1_Maria/src/main/java/com/katheprc/TP1/service/ProfesorService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.katheprc.TP1.entity.Profesor;
 import com.katheprc.TP1.repository.ProfesorRepository;
@@ -20,4 +21,17 @@ public class ProfesorService {
 		return listaProfesores;
 	}
 
+	@Transactional
+	public void guardar(String nombre, String materia, String turno) {
+		
+		Profesor profe = new Profesor();
+		
+		profe.setMateria(materia);
+		profe.setNombre(nombre);
+		profe.setTurno(turno);
+		
+		pRepo.save(profe);
+	}
+
 }
+
